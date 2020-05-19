237d5d8a25083e3e212504f5f7adf67d5f632877
David Hatton <dhatton@turnitin.com>
2020-05-19 16:54:49 +0100

    Asynchronous fetching

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
+import {fetchPosts} from "../actions";
 
 class PostList extends Component {
-  render() {
-    return (
-        <div>
-          Post List
-        </div>
-    );
-  }
+  componentDidMount = () => {
+    this.props.fetchPosts();
+  };
+
+  render = () => (
+      <div>
+        Post List
+      </div>
+  );
 }
 
-export default PostList;
+const mapStateToProps = state => {
+  // posts: state.posts;
+};
+
+export default connect(mapStateToProps, {fetchPosts})(PostList);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
+import thunk from "redux-thunk";
 
 import './index.css';
 import App from './components/App';
 import reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(
+    reducers,
+    compose(applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={createStore(reducers)}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
