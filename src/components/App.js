caa91b2025924cd904a43513bd0b4456e90b11c8
David Hatton <dhatton@turnitin.com>
2020-05-19 11:59:28 +0100

    Add PostList

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import PostList from "./PostList";
 
 function App() {
   return (
     <div className="App ui container">
-      This is the app
+      <PostList />
     </div>
   );
 }
